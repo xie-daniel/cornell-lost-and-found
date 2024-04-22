ff65c9ee27b06f702f48f5a1c85f2a23ce33c0d1
Daniel Xie <dyx2@cornell.edu>
2024-04-22 17:59:28 -0400

    Implemented grid layout for browsing lost items
    - 3 column grid layout in the Browse page
    - Used an array of dummy data to display

--- frontend/src/pages/Browse.tsx
+++ frontend/src/pages/Browse.tsx
@@ -1,7 +1,17 @@
-const BrowsePage = () => (
-    <center>
+import {Card} from "@mantine/core";
+
+const BrowsePage = () => {
+    const lostItems = ["Water Bottle", 'Backpack','iPhone 13', 'AirPods', 'Student ID', 'Hydroflask', 'Necklace', 'Ring'];
+    
+    return (
+    <div style={{display:"flex", justifyContent:"center", flexDirection: "column", alignItems: "center"}}>
         <h1>Browse Lost Items</h1>
-    </center>
-);
+        <div style={{display: "grid", gap: "20px", gridTemplateColumns: "repeat(3, 1fr)"}}>
+            {lostItems.map((item) => <Card style={{border: "2px solid black", width: "400px", height: "250px"}}>
+                <h3>{item}</h3>
+            </Card>)}
+        </div>
+    </div>
+)};
 
 export default BrowsePage;
