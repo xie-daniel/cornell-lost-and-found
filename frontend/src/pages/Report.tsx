047feec8d5c7f4dbc444f065b5d99d015e12a118
Leon Huang <lyh7@cornell.edu>
2024-04-22 19:32:14 -0400

    Implemented frontend for Report.tsx

--- frontend/src/pages/Report.tsx
+++ frontend/src/pages/Report.tsx
@@ -7,21 +7,40 @@ const getWeather = (): Promise<WeatherResponse> =>
     fetch(`${BACKEND_BASE_PATH}/weather`).then((res) => res.json());
 
 const Report = () => {
-    const [{ raining }, setRaining] = useState<WeatherResponse>({
-        raining: coinflip(),
-    });
-
-    useEffect(() => {
-        console.log("Loading weather...");
-        getWeather().then((data) => setRaining(data));
-    }, []);
-
     return (
-        <center>
-            <h1>Report Lost Item</h1>
-            <p>{raining ? "Yes" : "No"}</p>
-        </center>
-    );
+        <div className="center">
+            <h1>    Submit Lost and Found Entry</h1>
+
+            <h2>    Name of Item
+                <div></div>
+            <input type="text" 
+            placeholder = {"E.g Water Bottle, Basketball, Airpods ..."} 
+            style={{width: "370px", fontSize: 14}}/>
+            </h2>
+
+            <h2>    Description of Item
+                <div></div>
+            <input type="text" 
+            placeholder = {"E.g Blue 64oz Hydro Flask"} 
+            style={{width: "370px", fontSize: 14}}/>
+            </h2>
+
+            <h2>    Location Found
+                <div></div>
+            <input type="text" 
+            placeholder = {"E.g Helen Newman Hall"} 
+            style={{width: "370px", fontSize: 14}}/>
+            </h2>
+
+            <h2>    Date Found
+            <div></div>
+            <input type="text" placeholder = {"MM/DD/YYYY"} 
+            style={{fontSize: 14}}/>
+            </h2>
+
+          <button>    Submit!</button>
+        </div>
+);
 };
 
 export default Report;
