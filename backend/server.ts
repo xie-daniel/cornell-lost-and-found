a03eca7e636aea0ddd7af40ec3d2a88197417503
Leon Huang <lyh7@cornell.edu>
2024-04-23 00:15:38 -0400

    established code in the backend folder

--- backend/server.ts
+++ backend/server.ts
@@ -11,34 +11,53 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 
-type WeatherData = {
-    latitude: number;
-    longitude: number;
-    timezone: string;
-    timezone_abbreviation: string;
-    current: {
-        time: string;
-        interval: number;
-        precipitation: number;
+type LostAndFoundItem = {
+    id : string; 
+    name: string;
+    description: string;
+    locationFound: string;
+    date: {
+        month: number;
+        day: number;
+        year: number;
     };
 };
 
-app.get("/weather", async (req, res) => {
-    console.log("GET /api/weather was called");
-    try {
-        const response = await fetch(
-            "https://api.open-meteo.com/v1/forecast?latitude=40.7411&longitude=73.9897&current=precipitation&temperature_unit=fahrenheit&windspeed_unit=mph&timezone=America%2FNew_York&forecast_days=1"
-        );
-        const data = (await response.json()) as WeatherData;
-        const output: WeatherResponse = {
-            raining: data.current.precipitation > 0.5,
-        };
-        res.json(output);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Something went wrong" });
-    }
+//replace later, for now just empty
+let lostItems : LostAndFoundItem[] = [];
+
+app.get("/lost-items", async (req, res) => {
+    res.json(lostItems);
 });
+
+app.post('/lost-items', (req, res) => {
+    const newItem = req.body;
+    lostItems.push(newItem);
+    res.json(lostItems)
+  });
+
+app.put('/api/lost-items/:id', (req, res) => {
+    const itemId = req.params.id;
+    const updatedItem : LostAndFoundItem = req.body;
+    const index = lostItems.findIndex(item => item.id === itemId);
+    if (index !== -1) {
+      lostItems[index] = updatedItem;
+      res.json(lostItems[index]);
+    } else {
+      res.status(404).json({ error: 'Lost item not found' });
+    }
+  });
+
+  app.delete('/api/lost-items/:id', (req, res) => {
+    const itemId = req.params.id;
+    const index = lostItems.findIndex(item => item.id === itemId);
+    if (index !== -1) {
+      const deletedItem = lostItems.splice(index, 1)[0];
+      res.json(deletedItem);
+    } else {
+      res.status(404).json({ error: 'Lost item not found' });
+    }
+  });
 
 app.listen(port, hostname, () => {
     console.log("Listening");
