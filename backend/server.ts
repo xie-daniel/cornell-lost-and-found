f11786a6e06ed624d7b245ed617ade4ba0976df9
Daniel Xie <dyx2@cornell.edu>
2024-04-23 21:42:10 -0400

    Made frontend data types more compatible with backend

--- backend/server.ts
+++ backend/server.ts
@@ -12,53 +12,54 @@ app.use(cors());
 app.use(express.json());
 
 type LostAndFoundItem = {
-    id : string; 
-    name: string;
-    description: string;
-    locationFound: string;
-    date: {
-        month: number;
-        day: number;
-        year: number;
-    };
+  id: string;
+  name: string;
+  description: string;
+  locationFound: string;
+  date: {
+    month: number;
+    day: number;
+    year: number;
+  };
+  claimed: boolean;
 };
 
 //replace later, for now just empty
-let lostItems : LostAndFoundItem[] = [];
+let lostItems: LostAndFoundItem[] = [];
 
 app.get("/lost-items", async (req, res) => {
-    res.json(lostItems);
+  res.json(lostItems);
 });
 
-app.post('/lost-items', (req, res) => {
-    const newItem = req.body;
-    lostItems.push(newItem);
-    res.json(lostItems)
-  });
+app.post("/lost-items", (req, res) => {
+  const newItem = req.body;
+  lostItems.push(newItem);
+  res.json(lostItems);
+});
 
-app.put('/api/lost-items/:id', (req, res) => {
-    const itemId = req.params.id;
-    const updatedItem : LostAndFoundItem = req.body;
-    const index = lostItems.findIndex(item => item.id === itemId);
-    if (index !== -1) {
-      lostItems[index] = updatedItem;
-      res.json(lostItems[index]);
-    } else {
-      res.status(404).json({ error: 'Lost item not found' });
-    }
-  });
+app.put("/api/lost-items/:id", (req, res) => {
+  const itemId = req.params.id;
+  const updatedItem: LostAndFoundItem = req.body;
+  const index = lostItems.findIndex((item) => item.id === itemId);
+  if (index !== -1) {
+    lostItems[index] = updatedItem;
+    res.json(lostItems[index]);
+  } else {
+    res.status(404).json({ error: "Lost item not found" });
+  }
+});
 
-  app.delete('/api/lost-items/:id', (req, res) => {
-    const itemId = req.params.id;
-    const index = lostItems.findIndex(item => item.id === itemId);
-    if (index !== -1) {
-      const deletedItem = lostItems.splice(index, 1)[0];
-      res.json(deletedItem);
-    } else {
-      res.status(404).json({ error: 'Lost item not found' });
-    }
-  });
+app.delete("/api/lost-items/:id", (req, res) => {
+  const itemId = req.params.id;
+  const index = lostItems.findIndex((item) => item.id === itemId);
+  if (index !== -1) {
+    const deletedItem = lostItems.splice(index, 1)[0];
+    res.json(deletedItem);
+  } else {
+    res.status(404).json({ error: "Lost item not found" });
+  }
+});
 
 app.listen(port, hostname, () => {
-    console.log("Listening");
+  console.log("Listening");
 });
--- frontend/src/pages/Browse.tsx
+++ frontend/src/pages/Browse.tsx
@@ -1,60 +1,111 @@
 import {Card} from "@mantine/core";
 
+// type Item = {
+//     itemName: string;
+//     location: string;
+//     dateReported: Date;
+//     claimed: boolean;
+//     description: string
+// }
+
 type Item = {
-    itemName: string;
-    location: string;
-    dateReported: Date;
+    id : string; 
+    name: string;
+    description: string;
+    locationFound: string;
+    date: {
+        month: number;
+        day: number;
+        year: number;
+    };
     claimed: boolean;
-    description: string
 }
 
 const BrowsePage = () => {
-    const lostItems: Item[] = [
+    const dummyData: Item[] = [
         {
-            itemName: "Water Bottle",
-            location: "Duffield Atrium",
-            dateReported: new Date('April 18, 2023 03:00:00'),
+            id: "1",
+            name: "Water Bottle",
+            description: "Pink Hydroflask found on one of the tables near Mattin's Cafe. I left it under the table.",
+            locationFound: "Duffield Atrium",
+            date: {
+                month: 4,
+                day: 20,
+                year: 2024
+            },
             claimed: false,
-            description: "Pink Hydroflask found on one of the tables near Mattin's Cafe. I left it under the table."
+            
         },
         {
-            itemName: "Laptop",
-            location: "Cocktail Lounge",
-            dateReported: new Date('April 22, 2023 01:00:00'),
+            id: "2",
+            name: "Laptop",
+            description: "MacBook Air, found on one of the sofas of the cocktail lounge. Contact me at placeholder@gmail.com to get it back.",
+            locationFound: "Cocktail Lounge",
+            date: {
+                month: 4,
+                day: 22,
+                year: 2024
+            },
+            claimed: true
+        },
+        {
+            id: "3",
+            name: "Student ID",
+            description: "Found an ID with the name \"Daniel Xie\" and gave it to the librarian. Speak to the Olin front desk to get it back.",
+            locationFound: "Olin Library",
+            date: {
+                month: 4,
+                day: 21,
+                year: 2024
+            },
+            claimed: true
+        },
+        {
+            id: "4",
+            name: "Apple Watch with blue wristband",
+            description: "I found it in an RPCC study pod. I gave it to the service center, talk to them to get it back.",
+            locationFound: "RPCC",
+            date: {
+                month: 4,
+                day: 15,
+                year: 2024
+            },
             claimed: true,
-            description: "MacBook Air, found on one of the sofas of the cocktail lounge. Contact me at placeholder@gmail.com to get it back."
         },
         {
-            itemName: "Student ID",
-            location: "Olin Library",
-            dateReported: new Date('April 21, 2023 01:00:00'),
+            id: "5",
+            name: "Black Sweatshirt",
+            description: "I left it at the tables outside Crossings because they were about to close.",
+            locationFound: "Crossings Cafe",
+            date: {
+                month: 4,
+                day: 23,
+                year: 2024
+            },
             claimed: false,
-            description: "Found an ID with the name \"Daniel Xie\" and gave it to the librarian. Speak to the Olin front desk to get it back."
-        },
-        {
-            itemName: "Apple Watch with blue wristband",
-            location: "RPCC",
-            dateReported: new Date('April 11, 2023 01:00:00'),
-            claimed: true,
-            description: "I found it in an RPCC study pod. I gave it to the service center, talk to them to get it back."
-        },
-        {
-            itemName: "Black Sweatshirt",
-            location: "Crossings Cafe",
-            dateReported: new Date('April 13, 2023 01:00:00'),
-            claimed: true,
-            description: "I left it at the tables outside Crossings because they were about to close."
         }
     ]
+
+    const BACKEND_BASE_PATH = 'http://localhost:8080/'
+
+    let lostItems: Item[] = []
+
+    const getLostItems = async () => {
+        const res = await fetch(`${BACKEND_BASE_PATH}/lost-items`);
+        const d = await res.json();
+        lostItems = d;
+    };
+
+    getLostItems();
 
     return (
     <div style={{display:"flex", justifyContent:"center", flexDirection: "column", alignItems: "center"}}>
         <h1>Browse Lost Items</h1>
         <div style={{display: "grid", gap: "20px", gridTemplateColumns: "repeat(2, 1fr)"}}>
-            {lostItems.map((item) => <Card style={{border: "1px solid black", width: "600px", height: "250px", position: "relative"}}>
-                <strong>{item.itemName}</strong>
-                <p>Found at: {item.location}</p>
-                <p>Found on: {item.dateReported.toDateString()}</p>
+            {dummyData.map((item) => <Card style={{border: "1px solid black", width: "600px", height: "250px", position: "relative"}}>
+                <strong>{item.name}</strong>
+                <p>Found at: {item.locationFound}</p>
+                <p>Found on: {item.date.month}/{item.date.day}/{item.date.year}</p>
                 <p>Claimed? {item.claimed ? "Yes" : "No"}</p>
                 <p>Description: {item.description}</p>
                 <div style={{position: "absolute", top: "25px", right: "25px"}}>
